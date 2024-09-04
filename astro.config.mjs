452c0219d280f353e974c46facf91e0c45ec5686
Mack Richardson <mack@mackrichardson.com>
2024-09-04 14:50:27 -0400

    Installed partytown, mdx and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://astro-article.netlify.app/',
+    integrations: [
+        mdx(), 
+        sitemap(),
+        partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),
+    ],
+});
